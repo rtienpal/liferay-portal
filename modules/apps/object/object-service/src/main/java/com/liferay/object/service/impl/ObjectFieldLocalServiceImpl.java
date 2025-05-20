5ac3e39839b7c703221885e3a2f75134f7d6c3b3
Jhosseph Gonzalez <jhossephgonzalez@gmail.com>
2025-05-20 09:03:26 -0300

    LPD-44846 Pass new reserved names in their expected locations

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFieldLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFieldLocalServiceImpl.java
@@ -1915,8 +1915,9 @@ public class ObjectFieldLocalServiceImpl
 		"createDate", "creator", "id", "modifiedDate", "status");
 	private final Set<String> _reservedNames = SetUtil.fromArray(
 		"actions", "companyid", "createdate", "creator", "currentdate",
-		"datecreated", "datemodified", "externalreferencecode", "groupid", "id",
-		"lastpublishdate", "modifieddate", "status", "statusbyuserid",
+		"datecreated", "datemodified", "expirationdate",
+		"externalreferencecode", "groupid", "id", "lastpublishdate",
+		"modifieddate", "publishdate", "reviewdate", "status", "statusbyuserid",
 		"statusbyusername", "statusdate", "userid", "username");
 
 	@Reference
