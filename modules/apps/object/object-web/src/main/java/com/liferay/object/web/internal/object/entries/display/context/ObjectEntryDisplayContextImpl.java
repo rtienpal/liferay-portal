a582498834679cfff39cf5a660891045f2f0a117
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2025-06-04 12:33:10 -0300

    LPD-44846 Optimize

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/entries/display/context/ObjectEntryDisplayContextImpl.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/entries/display/context/ObjectEntryDisplayContextImpl.java
@@ -855,13 +855,9 @@ public class ObjectEntryDisplayContextImpl
 			ObjectEntry objectEntry, String fieldName)
 		throws PortalException {
 
-		if (objectEntry == null) {
-			return JSONUtil.put("checked", true);
-		}
+		if ((objectEntry == null) ||
+			(objectEntry.getPropertyValue(fieldName) == null)) {
 
-		Object value = objectEntry.getPropertyValue(fieldName);
-
-		if (value == null) {
 			return JSONUtil.put("checked", true);
 		}
 
@@ -876,7 +872,7 @@ public class ObjectEntryDisplayContextImpl
 					_objectFieldLocalService.getObjectField(
 						objectDefinition.getObjectDefinitionId(), fieldName),
 					HashMapBuilder.put(
-						fieldName, value
+						fieldName, objectEntry.getPropertyValue(fieldName)
 					).build());
 			}
 		);
