63175c56b6f2e5a7df7fa6be5466d7f862308aa3
Brian Greenwald <brian.greenwald@liferay.com>
2025-06-05 11:53:56 -0500

    LPD-56243 Add JakartaCompatabilityUtil and test

---
+++ modules/sdk/project-templates/project-templates/src/main/java/com/liferay/project/templates/internal/util/JakartaCompatabilityUtil.java
@@ -0,0 +1,63 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.project.templates.internal.util;
+
+import com.liferay.project.templates.extensions.util.FileUtil;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+/**
+ * @author Brian Greenwald
+ */
+public class JakartaCompatabilityUtil {
+
+	public static void updateForJakarta(File destinationDir) throws Exception {
+		Files.walkFileTree(
+			destinationDir.toPath(),
+			new SimpleFileVisitor<Path>() {
+
+				@Override
+				public FileVisitResult visitFile(
+						Path path, BasicFileAttributes basicFileAttributes)
+					throws IOException {
+
+					String fileName = String.valueOf(path.getFileName());
+
+					if (Files.isRegularFile(path)) {
+						File file = path.toFile();
+
+						FileUtil.replaceString(
+							file, _JAVAX_STRING, _JAKARTA_STRING);
+
+						if (fileName.endsWith(".jsp")) {
+							FileUtil.replaceString(
+								file, _OLD_TAGLIB_URL, _JAKARTA_TAGLIB_URL);
+						}
+					}
+
+					return FileVisitResult.CONTINUE;
+				}
+
+			});
+	}
+
+	private static final String _JAKARTA_STRING = "jakarta";
+
+	private static final String _JAKARTA_TAGLIB_URL = "jakarta.tags.core";
+
+	private static final String _JAVAX_STRING = "javax";
+
+	private static final String _OLD_TAGLIB_URL =
+		"http://java.sun.com/jsp/jstl/core";
+
+}
---
+++ modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/internal/util/JakartaCompatabilityUtilTest.java
@@ -0,0 +1,123 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.project.templates.internal.util;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.project.templates.extensions.util.FileUtil;
+
+import java.io.IOException;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Brian Greenwald
+ */
+public class JakartaCompatabilityUtilTest {
+
+	@Before
+	public void setUp() throws Exception {
+		_sourceTemplatesDir = Paths.get(
+			"src/test/resources/com/liferay/project/templates/internal/util" +
+				"/templates");
+		_tempDir = Files.createTempDirectory("jakarta-test");
+
+		Files.walkFileTree(
+			_sourceTemplatesDir,
+			new SimpleFileVisitor<Path>() {
+
+				@Override
+				public FileVisitResult visitFile(
+						Path path, BasicFileAttributes basicFileAttributes)
+					throws IOException {
+
+					String updatedName = _sourceTemplatesDir.relativize(
+						path
+					).toString(
+					).replace(
+						"_template", StringPool.BLANK
+					);
+
+					Path destinationPath = _tempDir.resolve(updatedName);
+
+					Files.copy(
+						path, destinationPath,
+						StandardCopyOption.REPLACE_EXISTING);
+
+					return FileVisitResult.CONTINUE;
+				}
+
+			});
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		if ((_tempDir != null) && Files.exists(_tempDir)) {
+			FileUtil.deleteDir(_tempDir);
+		}
+	}
+
+	@Test
+	public void testUpdateForJakarta() throws Exception {
+		JakartaCompatabilityUtil.updateForJakarta(_tempDir.toFile());
+
+		Files.walkFileTree(
+			_tempDir,
+			new SimpleFileVisitor<Path>() {
+
+				@Override
+				public FileVisitResult visitFile(
+					Path path, BasicFileAttributes basicFileAttributes) {
+
+					try {
+						String fileName = path.getFileName(
+						).toString();
+
+						String expectedFileName = fileName + "_expected";
+
+						Path expectedFilePathInResources =
+							_sourceTemplatesDir.getParent(
+							).resolve(
+								expectedFileName
+							);
+
+						Assert.assertTrue(
+							"Expected file does not exist: " +
+								expectedFilePathInResources,
+							Files.exists(expectedFilePathInResources));
+
+						String processedFileContent = Files.readString(path);
+						String expectedFileContent = Files.readString(
+							expectedFilePathInResources);
+
+						Assert.assertEquals(
+							"File content does not match for " + fileName,
+							expectedFileContent, processedFileContent);
+					}
+					catch (IOException ioException) {
+						throw new RuntimeException(ioException);
+					}
+
+					return FileVisitResult.CONTINUE;
+				}
+
+			});
+	}
+
+	private Path _sourceTemplatesDir;
+	private Path _tempDir;
+
+}
