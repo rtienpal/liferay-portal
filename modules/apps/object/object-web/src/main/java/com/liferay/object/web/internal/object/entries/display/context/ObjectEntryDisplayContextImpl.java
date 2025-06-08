d47a1298efec10ebf6714f4dfb9a6d0395917b42
Marcela Cunha <marcela.cunha@liferay.com>
2025-06-08 18:00:54 -0300

    LPD-55251 Rename

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/entries/display/context/ObjectEntryDisplayContextImpl.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/entries/display/context/ObjectEntryDisplayContextImpl.java
@@ -580,10 +580,11 @@ public class ObjectEntryDisplayContextImpl
 
 		return HashMapBuilder.<String, Object>put(
 			"expirationDate",
-			() -> _createScheduleProperty(objectEntry, "expirationDate")
+			() -> _createSchedulePropertyJSONObject(
+				objectEntry, "expirationDate")
 		).put(
 			"reviewDate",
-			() -> _createScheduleProperty(objectEntry, "reviewDate")
+			() -> _createSchedulePropertyJSONObject(objectEntry, "reviewDate")
 		).build();
 	}
 
@@ -851,7 +852,7 @@ public class ObjectEntryDisplayContextImpl
 		return objectFieldRenderingContext;
 	}
 
-	private JSONObject _createScheduleProperty(
+	private JSONObject _createSchedulePropertyJSONObject(
 			ObjectEntry objectEntry, String fieldName)
 		throws PortalException {
 
