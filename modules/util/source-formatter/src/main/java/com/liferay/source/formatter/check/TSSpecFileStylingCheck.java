a6e4ced10c67c60c0aba306233793db4000d47c9
Alan Huang <alan.huang@liferay.com>
2025-06-10 15:27:51 +0800

    LPD-57521 Use single quote when possible

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/TSSpecFileStylingCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/TSSpecFileStylingCheck.java
@@ -5,6 +5,8 @@
 
 package com.liferay.source.formatter.check;
 
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 
 import java.util.regex.Matcher;
@@ -35,14 +37,26 @@ public class TSSpecFileStylingCheck extends BaseFileCheck {
 		while (matcher.find()) {
 			String description = matcher.group(1);
 
-			String trimmedDescription = description.trim();
+			String unquotedDescription = StringUtil.unquote(description);
 
-			if (description.equals(trimmedDescription)) {
+			String trimmedDescription = unquotedDescription.trim();
+
+			String newDescription = null;
+
+			if (trimmedDescription.indexOf(CharPool.APOSTROPHE) == -1) {
+				newDescription = StringUtil.quote(trimmedDescription);
+			}
+			else {
+				newDescription = StringUtil.quote(
+					trimmedDescription, StringPool.QUOTE);
+			}
+
+			if (description.equals(newDescription)) {
 				continue;
 			}
 
 			String replacement = StringUtil.replaceFirst(
-				matcher.group(), description, trimmedDescription);
+				matcher.group(), description, newDescription);
 
 			matcher.appendReplacement(sb, replacement);
 		}
@@ -57,6 +71,6 @@ public class TSSpecFileStylingCheck extends BaseFileCheck {
 	}
 
 	private static final Pattern _descriptionPattern = Pattern.compile(
-		"\n\t*test\\(\\s*'(.+)',\\s");
+		"\n\t*test\\(\\s*((['\"]).+\\2),\\s");
 
 }
