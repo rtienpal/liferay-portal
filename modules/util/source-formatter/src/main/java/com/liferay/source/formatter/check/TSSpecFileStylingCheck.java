090e4c09990e2100cf1aef6d6c9382f8eef1556f
Alan Huang <alan.huang@liferay.com>
2025-06-10 14:13:33 +0800

    LPD-57521 Add TSSpecFileStylingCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/TSSpecFileStylingCheck.java
@@ -0,0 +1,62 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.source.formatter.check;
+
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Alan Huang
+ */
+public class TSSpecFileStylingCheck extends BaseFileCheck {
+
+	@Override
+	public boolean isLiferaySourceCheck() {
+		return true;
+	}
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, String content) {
+
+		if (!fileName.endsWith(".spec.ts")) {
+			return content;
+		}
+
+		StringBuffer sb = new StringBuffer();
+
+		Matcher matcher = _descriptionPattern.matcher(content);
+
+		while (matcher.find()) {
+			String description = matcher.group(1);
+
+			String trimmedDescription = description.trim();
+
+			if (description.equals(trimmedDescription)) {
+				continue;
+			}
+
+			String replacement = StringUtil.replaceFirst(
+				matcher.group(), description, trimmedDescription);
+
+			matcher.appendReplacement(sb, replacement);
+		}
+
+		if (sb.length() > 0) {
+			matcher.appendTail(sb);
+
+			return sb.toString();
+		}
+
+		return content;
+	}
+
+	private static final Pattern _descriptionPattern = Pattern.compile(
+		"\n\t*test\\(\\s*'(.+)',\\s");
+
+}
