44be215b3ca5820f0e7ba36fc48663f203ff93f4
Rafael Uen <rafael.uen@liferay.com>
2025-11-25 19:13:49 -0300

    LPD-49589 Implement enable default values for boolean fields in object admin

---
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectField/DefaultValueFields/NumericDefaultValueInput.tsx
@@ -0,0 +1,84 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {NumericEntryBaseField} from '@liferay/object-js-components-web';
+import React, {useState} from 'react';
+import {createNumberMask} from 'text-mask-addons';
+import {conformToMask} from 'text-mask-core';
+
+import {getUpdatedDefaultValueFieldSettings} from '../../../utils/defaultValues';
+import {InputAsValueFieldComponentProps} from '../Tabs/Advanced/DefaultValueContainer';
+
+const NumericDefaultValueInput: React.FC<
+	{children?: React.ReactNode | undefined} & InputAsValueFieldComponentProps
+> = ({
+	dataType,
+	defaultValue,
+	error,
+	label,
+	onSubmit,
+	required,
+	setValues,
+	values,
+}: InputAsValueFieldComponentProps) => {
+	const [value, setValue] = useState(defaultValue as string);
+	const handleChangeInput = (event: any) => {
+		const config = {
+			allowDecimal: dataType === 'double',
+			allowLeadingZeroes: true,
+			allowNegative: true,
+			decimalLimit: null,
+			decimalSymbol: '.',
+			includeThousandsSeparator: false,
+			prefix: '',
+		};
+
+		const mask = createNumberMask(config);
+
+		const {conformedValue: masked} = conformToMask(
+			event.target.value,
+			mask,
+			{
+				guide: false,
+				keepCharPositions: false,
+				placeholderChar: '\u2000',
+			}
+		);
+
+		if (masked) {
+			const newObjectFieldSettings = getUpdatedDefaultValueFieldSettings(
+				values,
+				masked,
+				'inputAsValue'
+			);
+
+			setValues({
+				objectFieldSettings: newObjectFieldSettings,
+			});
+
+			if (onSubmit) {
+				onSubmit({
+					...values,
+					objectFieldSettings: newObjectFieldSettings,
+				});
+			}
+		}
+
+		setValue(masked);
+	};
+
+	return (
+		<NumericEntryBaseField
+			error={error}
+			label={label}
+			onChange={handleChangeInput}
+			placeholder={Liferay.Language.get('choose-an-option')}
+			required={required}
+			value={value}
+		/>
+	);
+};
+
+export default NumericDefaultValueInput;
